205e54409a9e3a1c851a4ab19a9381ce27d7138a
Christoph Flick <christophflick@gmx.de>
2019-10-13 10:50:18 +0200

    Add scope info type to the documentation

--- src/documentation/types.ts
+++ src/documentation/types.ts
@@ -1,5 +1,16 @@
 import { CompletionItemKind, MarkdownString } from 'vscode';
 
+export type Scope = 'source.firebase' |
+    'comment.line' |
+    'meta.root.fs' |
+    'meta.matcher.fs' |
+    'string.unquoted.fs' |
+    'meta.function.fs' |
+    'meta.function.expression.fs' |
+    'meta.allow.fs' |
+    'meta.functioncall.fs' |
+    'string.quoted.firestorerules';
+
 export type FlatDoc = { [name: string]: string | MarkdownString };
 
 export interface DocumentationValue {
@@ -7,6 +18,7 @@ export interface DocumentationValue {
     doc: string | MarkdownString;
     kind?: CompletionItemKind;
     childs?: Documentation;
+    scopes?: Scope[];
 }
 
 export type Documentation = { [name: string]: DocumentationValue };
