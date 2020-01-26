4127dd02f114c61241e044b12c5031d16d629767
Christoph Flick <christophflick@gmx.de>
2020-01-26 11:03:05 +0100

    Add a basic formatter
    Can indentate correct rules, does nothing else.

--- src/providers/FirestoreFormattingProvider.ts
+++ src/providers/FirestoreFormattingProvider.ts
@@ -1,22 +1,48 @@
-import { ProviderResult, TextEdit, CompletionItem, CompletionItemKind, DocumentFormattingEditProvider, CompletionList, Position, TextDocument, FormattingOptions, Range } from 'vscode';
-import { getPotentialDocForPartial, getPotentialDocForPartialScoped } from '../Documentation';
+import { DocumentFormattingEditProvider, FormattingOptions, Position, Range, TextDocument, TextEdit } from 'vscode';
 import { tokenize } from '../utils/textmate/textmate';
 
+const intendScopes = ['meta.root.fs', 'meta.matcher.fs', 'meta.function.expression.fs']
+const reduceWith = /match|service|^\s*\}\s*$/g;
+const concatedAndOr = /^\s*(&&|\|\|)/g;
+
 export class FirestoreFormattingProvider implements DocumentFormattingEditProvider {
+
     async provideDocumentFormattingEdits(document: TextDocument, options: FormattingOptions): Promise<TextEdit[]> {
         let results: TextEdit[] = [];
 
         try {
             const tokenizedDoc = await tokenize(document);
             tokenizedDoc.forEach((token, line) => {
-                const numberOfIntendation = Math.max(...token.map(
+                let numberOfIntendation = Math.max(...token.map(
                     item => item.scopes.reduce(
-                        (count, scope) => scope === 'meta.matcher.fs' ? count + 1 : count, 0
+                        (count, scope) => intendScopes.includes(scope) ? count + 1 : count, 0
                     )
                 ));
-                const intendChar = options.insertSpaces ? ' '.repeat(options.tabSize || 2) : '\t';
 
-                results.push(new TextEdit(new Range(new Position(line, 0), new Position(line, 0)), intendChar.repeat(numberOfIntendation)));
+                /**
+                 * Do not indent match, service and closing bracket lines
+                 */
+                if (document.lineAt(line).text.match(reduceWith)) {
+                    numberOfIntendation--;
+                }
+
+                /**
+                 * Add an indentation step if line starts with && or ||
+                 */
+                if (document.lineAt(line).text.match(concatedAndOr)) {
+                    numberOfIntendation++;
+                }
+
+                const currentNumberOfIndentationWhitespace = document.lineAt(line).text.length - document.lineAt(line).text.trimLeft().length;
+                const intendationSize = options.tabSize || 2;
+                const intendChar = options.insertSpaces ? ' '.repeat(intendationSize) : '\t';
+
+                results.push(
+                    TextEdit.replace(
+                        new Range(new Position(line, 0), new Position(line, currentNumberOfIndentationWhitespace)),
+                        intendChar.repeat(numberOfIntendation)
+                    )
+                );
             });
 
         } catch (error) {
