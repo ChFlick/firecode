a15ada8ab05daa709f9a173fa72fd11794a82cde
Christoph Flick <christophflick@gmx.de>
2020-02-25 15:09:33 +0100

    Add another autoformatter test

--- src/test/suite/autoformatter/autoformatter.test.ts
+++ src/test/suite/autoformatter/autoformatter.test.ts
@@ -8,16 +8,29 @@ describe('the autoformatter', () => {
             .openTextDocument(__dirname + '/../../../../src/test/suite/autoformatter/matcherNoSemicolon.formatted.rules');
         const document = await vscode.workspace
             .openTextDocument(__dirname + '/../../../../src/test/suite/autoformatter/matcherNoSemicolon.test.rules');
-        const editor = await vscode.window.showTextDocument(document);
 
-        const formatEdits = await vscode.commands.executeCommand(
-            'vscode.executeFormatDocumentProvider',
-            document.uri,
-            { tabSize: 2, insertSpaces: true }
-        ) as vscode.TextEdit[];
-        await editor.edit(editBuilder => formatEdits.forEach(edit => editBuilder.replace(edit.range, edit.newText)));
-        const formattedResult = editor.document.getText();
+        const formattedResult = await getDocumentFormatted(document);
+
+        assert.strictEqual(formattedResult, formattedDocument.getText());
+    });
+
+    test('should format some complex rules', async () => {
+        const formattedDocument = await vscode.workspace
+            .openTextDocument(__dirname + '/../../../../src/test/suite/autoformatter/complex.formatted.rules');
+        const document = await vscode.workspace
+            .openTextDocument(__dirname + '/../../../../src/test/suite/autoformatter/complex.test.rules');
+
+        const formattedResult = await getDocumentFormatted(document);
 
         assert.strictEqual(formattedResult, formattedDocument.getText());
     });
 });
+
+async function getDocumentFormatted(document: vscode.TextDocument) {
+    const editor = await vscode.window.showTextDocument(document);
+    const formatEdits = await vscode.commands.executeCommand('vscode.executeFormatDocumentProvider', document.uri, { tabSize: 2, insertSpaces: true }) as vscode.TextEdit[];
+    await editor.edit(editBuilder => formatEdits.forEach(edit => editBuilder.replace(edit.range, edit.newText)));
+    const formattedResult = editor.document.getText();
+    return formattedResult;
+}
+
