94d102896ce2fe73d1e4cf9ca44edcaa589f1302
Christoph Flick <christophflick@gmx.de>
2020-05-12 15:25:08 +0200

    Remove sample test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -10,9 +10,4 @@ suite('Extension Test Suite', () => {
 	before(() => {
 		vscode.window.showInformationMessage('Start all tests.');
 	});
-
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
 });
